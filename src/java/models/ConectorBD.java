9553f22be751ea11d5120db1732bb38a3a8550ee
morodo95 <twbgames@gmail.com>
2017-10-22 05:20:16 -0300

    CAMbios en el conectorBD, problema solucionado con xubuntu, carga de datos

--- src/java/models/ConectorBD.java
+++ src/java/models/ConectorBD.java
@@ -11,9 +11,11 @@ public class ConectorBD {
     final String CONTRASEÑA = "";
     Statement state;
     public ConectorBD() throws ClassNotFoundException {
+        System.out.println("Entrando al metodo ConectorBD");
         try {
             Class.forName("com.mysql.jdbc.Driver");
             conexion = (Connection) DriverManager.getConnection(URL_BASEDATOS, USUARIO, CONTRASEÑA);
+            System.out.println("Conexión exitosa");
         } catch (SQLException ex) {
             System.out.println("Conexión fallada");
         }
@@ -23,11 +25,13 @@ public class ConectorBD {
         Statement statement;
         try {
             String sentencia;
-            sentencia = "INSERT INTO `alumno`(`id`, `apellido`,`nombre`) VALUES (null,'"+apellido+"','"+nombre+"')";
+            sentencia = "INSERT INTO `Alumno` (`idAlumno`, `apellido`, `nombre`, `tipodoc`, `dni`, `provincia`, `departamento`, `extranjero`, `nacionalidad`, `nacimiento`, `sexo`, `obrasocial`, `becado`, `segurovida`, `establecimiento`, `egreso`, `jurisdiccion`, `establecimientosecundario`, `titulosecundario`, `egresosecundario`, `adeudaasignatura`, `cuantasasignaturas`, `juridisccionescuela`, `estudios`, `estudiosuper`) VALUES (NULL, '"+apellido+"', '"+nombre+"', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);";
+  
             statement = this.conexion.createStatement();
             statement.executeUpdate(sentencia);
             System.out.println("Realizados");
         } catch (SQLException ex) {
+            System.out.println("No se pudo ingresar los datos error: "+ ex);
         }
     }
     
