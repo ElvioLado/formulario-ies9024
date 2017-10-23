eabd6c73b1fd224af752f476b9849546311003ad
morodo95 <twbgames@gmail.com>
2017-10-23 03:52:52 -0300

    Agregue argumentos y modifique los dos archivos para que ingresara mas datos -HAsta ahora ingresa seguro de vida.

--- src/java/servlet/mostrarDatos.java
+++ src/java/servlet/mostrarDatos.java
@@ -19,7 +19,7 @@ public class mostrarDatos extends HttpServlet {
         try (PrintWriter out = response.getWriter()) {
             ConectorBD conector = new ConectorBD();
             Enumeration<String> array = request.getParameterNames();
-            String [] alumno = new String[23];
+            String [] alumno = new String[14];
             
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -32,18 +32,16 @@ public class mostrarDatos extends HttpServlet {
             out.println("<h2>IES 9-024 - <span>INSCRIPCIÒN </span>- SISTEMAS <span>Finalizado</span></h2>");
             out.println("<body>");
             out.println("<img src=\"./img/LOGO_NUEVO.png\" >");
-            
-            for(int x=0;x<24;x++){
+            int p=0;
+            for(int x=0;x<14;x++){
                 alumno[x] =  request.getParameter(array.nextElement());                
+                out.println("Dato :" +p+" = "+alumno[x]);
+                p++;
             }
-            out.println("<h2 style=\"color:brown\">Registro exitoso "+alumno[1]+ " "+alumno[0]+"</h2>");
-            out.println("<h2 style=\"color:brown\">Dato n°8 "+alumno[8]+"</h2>");
-            out.println("<h2 style=\"color:brown\">Dato n°9"+alumno[9]+"</h2>");
-            out.println("<h2 style=\"color:brown\">Dato n°10 "+alumno[10]+"</h2>");
-            out.println("<h2 style=\"color:brown\">Dato n°22 "+alumno[22]+"</h2>");
-  
-            conector.insertar(alumno[0],alumno[1],alumno[2],alumno[3],alumno[4],alumno[5],alumno[6],alumno[7],alumno[8],alumno[9],alumno[10],alumno[11],alumno[12],alumno[13],alumno[14],alumno[15],alumno[16],alumno[17],alumno[18],alumno[19],alumno[20],alumno[21],alumno[22],alumno[23]);
-            out.println("<h2 style=\"color:brown\">Obrasocial: "+alumno[10]+"</h2>");
+            out.println("<h2 style=\"color:brown\">Registro exitoso "+alumno[2]+ " "+alumno[1]+"</h2>");
+            
+            conector.insertar(alumno[0],alumno[1],alumno[2],alumno[3],alumno[4],alumno[5],alumno[6],alumno[7],alumno[8],alumno[9],alumno[10],alumno[11],alumno[12],alumno[13]);
+            
             out.println("<img src='https://media.tenor.com/images/be2fb8bdbd649cf6d57844340167cbdb/tenor.gif'>");
             
             out.println("</body>");
